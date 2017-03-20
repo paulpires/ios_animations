202094267cdffd5ab9fc5c0f39e2dc6b930696fb
Paul Pires <paulpires101@gmail.com>
2017-03-20 23:30:56 +0000

    add a spring animation on the screenshot (WIP)

--- Twitter_animation.playground/Contents.swift
+++ Twitter_animation.playground/Contents.swift
@@ -15,7 +15,7 @@ mask.contents = UIImage(named: "twitter_logo_t")?.cgImage
 mask.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
 mask.position = CGPoint(x: screenShotImageView.frame.size.width/2, y: screenShotImageView.frame.size.height/2)
 
-screenShotImageView.layer.mask = mask
+//screenShotImageView.layer.mask = mask
 
 let animation = CAKeyframeAnimation(keyPath: "bounds")
 animation.duration = 1.5
@@ -29,21 +29,26 @@ let boundsChanges = [
 animation.values = boundsChanges
 animation.keyTimes = [0, 0.7, 1]
 
-//CASpringAnimation ?
-class Delegate: NSObject, CAAnimationDelegate {
-    
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        screenShotImageView.layer.mask = nil
-        screenShotImageView.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
-        UIView.animate(withDuration: 0.5, delay: 0.05, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: [], animations: {
-            screenShotImageView.transform = CGAffineTransform(scaleX: 1, y: 1)
-        }, completion: nil)
-    }
-}
+let spring = CASpringAnimation(keyPath: "transform.scale")
+spring.duration = 1
+spring.damping = 20
+spring.initialVelocity = 0.5
+spring.fromValue = 1.10
+spring.toValue = 1
+spring.repeatCount = Float(INT_MAX)
 
-animation.delegate = Delegate()
-screenShotImageView.transform = CGAffineTransform(scaleX: 1.03, y: 1.03)
-mask.add(animation, forKey: "bounds")
+//screenShotImageView.transform = CGAffineTransform(scaleX: 1.03, y: 1.03)
+
+let fade = CAKeyframeAnimation(keyPath: "opacity")
+fade.duration = 1
+fade.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
+fade.values = [Float(0)]
+fade.keyTimes = [0]
+//fade.repeatCount = Float(INT_MAX)
+
+//mask.add(animation, forKey: "bounds")
+screenShotImageView.layer.add(spring, forKey: "transform.scale")
+//mask.add(fade, forKey: "opacity")
 
 PlaygroundPage.current.liveView = containerView
 PlaygroundPage.current.needsIndefiniteExecution = true
