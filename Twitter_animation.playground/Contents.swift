07b6e71d036c20cfb8429ff3fdac4d8d26129311
Pires, Paul <paul.pires@sky.uk>
2017-03-21 17:03:52 +0000

    change mask bounds at end of animation

--- Twitter_animation.playground/Contents.swift
+++ Twitter_animation.playground/Contents.swift
@@ -15,7 +15,7 @@ mask.contents = UIImage(named: "twitter_logo_t")?.cgImage
 mask.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
 mask.position = CGPoint(x: screenShotImageView.frame.size.width/2, y: screenShotImageView.frame.size.height/2)
 
-//screenShotImageView.layer.mask = mask
+screenShotImageView.layer.mask = mask
 
 let animation = CAKeyframeAnimation(keyPath: "bounds")
 animation.duration = 1.5
@@ -35,20 +35,18 @@ spring.damping = 20
 spring.initialVelocity = 0.5
 spring.fromValue = 1.10
 spring.toValue = 1
-spring.repeatCount = Float(INT_MAX)
+//spring.repeatCount = Float(INT_MAX)
 
-//screenShotImageView.transform = CGAffineTransform(scaleX: 1.03, y: 1.03)
+animation.beginTime = 0.0
+spring.beginTime = animation.duration
 
-let fade = CAKeyframeAnimation(keyPath: "opacity")
-fade.duration = 1
-fade.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-fade.values = [Float(0)]
-fade.keyTimes = [0]
-//fade.repeatCount = Float(INT_MAX)
+let group = CAAnimationGroup()
+group.duration = 10
+group.animations = [animation, spring]
 
-//mask.add(animation, forKey: "bounds")
+mask.add(animation, forKey: "bounds")
+mask.bounds = CGRect(x: 0, y: 0, width: 5000, height: 5000)
 screenShotImageView.layer.add(spring, forKey: "transform.scale")
-//mask.add(fade, forKey: "opacity")
 
 PlaygroundPage.current.liveView = containerView
 PlaygroundPage.current.needsIndefiniteExecution = true
